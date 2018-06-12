f13f45e7a201c9a3b7afe50198f49dbd15c7cd14
x3develop <x3develop@gmail.com>
2018-06-12 12:00:20 +0700

    cherry

--- _footer.php
+++ _footer.php
@@ -8,16 +8,16 @@
 ?>
 <div class="container text-muted text-center">
     <ul class="list-inline social-buttons">
-        <li><a href="#"><i class="fa fa-facebook sr-icons"></i></a>
+        <li><a href="https://www.facebook.com/groups/233275286816459/" target="_blank"><i class="fa fa-facebook sr-icons"></i></a>
         </li>
-        <li><a href="#"><i class="fa fa-twitter sr-icons"></i></a>
-        </li>
-        <li><a href="#"><i class="fa fa-google-plus sr-icons"></i></a>
-        </li>
+<!--        <li><a href="#"><i class="fa fa-twitter sr-icons"></i></a>-->
+<!--        </li>-->
+<!--        <li><a href="#"><i class="fa fa-google-plus sr-icons"></i></a>-->
+<!--        </li>-->
     </ul>
     <ul class="list-inline">
         <li class="footer-number"><i class="fa fa-phone sr-icons"></i>  (00228)92229954 </li>
         <li><i class="fa fa-envelope sr-icons"></i>  kouvenceslas93@gmail.com</li>
     </ul>
-    <p>Photography Fanatic Template &copy; 2017</p>
+    <p>Photography Fanatic Template &copy; 2018</p>
 </div>
