00a1b910d0c7faee9afb778267e1e334853e06eb
x3develop <x3develop@gmail.com>
2018-07-09 15:50:01 +0700

    cherry

--- controller/_phpMyAdminController.php
+++ controller/_phpMyAdminController.php
@@ -6,30 +6,34 @@
  * Time: 12:23 PM
  */
 
-require_once __DIR__."/_phpMyAdminModel.php";
+require_once __DIR__ . "/_phpMyAdminModel.php";
 
 $AM = new _phpMyAdminModel();
 
-$TABLETHIS = isset($_REQUEST['table'])?$_REQUEST['table']:'';
-
+$TABLETHIS = isset($_REQUEST['table']) ? $_REQUEST['table'] : '';
 
 
 $TABLELIST = [];
 $result = $AM->getTables();
-if(count($result) > 0){
+if (count($result) > 0) {
     $TABLELIST = $result;
 }
 
 
 $ATTRIBUTES = [];
-$result = $AM->getAttribute($TABLETHIS);
-if(count($result)>0){
-    $ATTRIBUTES = $result;
+if ($TABLETHIS != '') {
+    $result = $AM->getAttribute($TABLETHIS);
+    if (count($result) > 0) {
+        $ATTRIBUTES = $result;
+    }
 }
 
 
 $DATALIST = [];
-$result = $AM->getDataAll($TABLETHIS);
-if(count($result)>0){
-    $DATALIST = $result;
+if ($TABLETHIS != '') {
+    $result = $AM->getDataAll($TABLETHIS);
+    if (count($result) > 0) {
+        $DATALIST = $result;
+    }
 }
+
